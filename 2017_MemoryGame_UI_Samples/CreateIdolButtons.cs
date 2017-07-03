1bb2ff629b0ac32c370f72232968cd4cd4ea8f3d
SabrinaKatharina <sabrina.kotzem@ymail.com>
2017-07-03 08:38:20 +0200

    add Memory Game UI samples

---
+++ 2017_MemoryGame_UI_Samples/CreateIdolButtons.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// creates all buttons with idols' names in the scroll list; if one button is clicked it shows the related idol's card collection
+public class CreateIdolButtons : MonoBehaviour {
+
+    public List<GameObject> idolButtons = new List<GameObject>();
+
+    public GameObject parent;
+    public GameObject idolButtonTemplate;
+
+    public AlbumCollection albumCollection;
+
+    private CardCollection cardCollection;
+
+
+    private void OnEnable()
+    {
+        GameInformation.Instance.GetComponent<LoadScenes>().OnSceneHasLoaded += Setup;
+    }
+
+    private void OnDisable()
+    {
+        GameInformation.Instance.GetComponent<LoadScenes>().OnSceneHasLoaded -= Setup;
+    }
+
+
+    //initialization
+    void Setup() {
+
+        cardCollection = GameInformation.Instance.GetComponent<CardCollectionContainer>().cardCollection;
+        int amountButtons = cardCollection.idolCardCollections.Count;
+
+        for(int i = 0; i < amountButtons; i++)
+        {
+            GameObject newButton = Instantiate(idolButtonTemplate);
+
+            // set the button as child of the scroll list
+            newButton.transform.SetParent(parent.transform, false);
+
+            // set button text to the idol's name
+            string idolName = cardCollection.idolCardCollections[i].idolName;
+            newButton.GetComponentInChildren<Text>().text = idolName;
+
+            int temp = i;
+            newButton.GetComponent<Button>().onClick.AddListener(() => { SetIdolIndex(temp); });
+        }
+
+	}
+
+    void SetIdolIndex(int index)
+    {
+        albumCollection.SetIdolIndex(index);
+    }
+}
---
+++ 2017_MemoryGame_UI_Samples/StartMenu.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.IO;
+
+public class StartMenu : MonoBehaviour {
+
+    public UnityEngine.UI.Text startButtonText;
+    public GameObject createNewGameButton;
+    public GameObject credits;
+    public GameObject startMenu;
+    public GameObject optionsMenu;
+    public UnityEngine.UI.Text optionMusicButtonText;
+    public UnityEngine.UI.Text optionCacheButtonText;
+
+    [Header("Changing Frame Color")]
+    public GameObject memoryCardTemplate;
+    public GameObject winningCardTemplate;
+    public Material blackFrameMaterial;
+    public Material whiteFrameMaterial;
+
+    private bool isMusicPlaying;
+    private bool isCachingActivated;
+
+    private void Start()
+    {
+        // check if there are cards saved already, then the continue button displays
+        if(CardCollectionContainer.Instance.cardCollection.idolCardCollections.Count > 0)
+        {
+            startButtonText.text = "CONTINUE";
+            createNewGameButton.SetActive(true);
+        }
+        else
+        {
+            startButtonText.text = "CREATE NEW GAME";
+        }
+        isMusicPlaying = true;
+        isCachingActivated = true;
+    }
+
+    private void OnEnable()
+    {
+        if (credits)
+        {
+            credits.GetComponentInChildren<MoveTextUp>().finishedMovingUp += DisableCredits;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (credits)
+        {
+            credits.GetComponentInChildren<MoveTextUp>().finishedMovingUp -= DisableCredits;
+        }
+    }
+
+    private void DisableCredits()
+    {
+        credits.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void StartGame()
+    {
+        GameInformation.Instance.GetComponent<LoadScenes>().StartGame();
+    }
+
+    public void CreateNewGame()
+    {
+        CardCollectionContainer.Instance.cardCollection.idolCardCollections.Clear();
+        CardCollectionContainer.Instance.SaveCardCollection();
+    }
+
+    public void ShowAlbum()
+    {
+        GameInformation.Instance.GetComponent<LoadScenes>().LoadAlbumScene();
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+        credits.GetComponentInChildren<MoveTextUp>().StartMovingUp();
+    }
+
+    public void OpenOptionsMenu()
+    {
+        startMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void HideOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    public void SetFrameColor(string color)
+    {
+        if (color.Equals("Black"))
+        {
+            memoryCardTemplate.GetComponent<Renderer>().material = blackFrameMaterial;
+            winningCardTemplate.GetComponent<Renderer>().material = blackFrameMaterial;
+}
+        else if (color.Equals("White"))
+        {
+            memoryCardTemplate.GetComponent<Renderer>().material = whiteFrameMaterial;
+            winningCardTemplate.GetComponent<Renderer>().material = whiteFrameMaterial;
+        }
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void ToggleBackgroundMusic()
+    {
+        if (isMusicPlaying)
+        {
+            GameInformation.Instance.backgroundMusic.Stop();
+            optionMusicButtonText.text = "ENABLE";
+        }
+        else
+        {
+            GameInformation.Instance.backgroundMusic.Play();
+            optionMusicButtonText.text = "DISABLE";
+        }
+        isMusicPlaying = !isMusicPlaying;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void ToggleCaching()
+    {
+        if (isCachingActivated)
+        {
+            GameInformation.Instance.cachingTexturesAllowed = false;
+            optionCacheButtonText.text = "ENABLE";
+        }
+        else
+        {
+            GameInformation.Instance.cachingTexturesAllowed = true;
+            optionCacheButtonText.text = "DISABLE";
+        }
+        isCachingActivated = !isCachingActivated;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void ClearCache()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath , "Cache");
+        if (Directory.Exists(path))
+        { 
+            Directory.Delete(path, true);
+        }
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
